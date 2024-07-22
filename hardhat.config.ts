6ea12d3e0af0d39c3109989365c55fb3bab94a85
akaMiWP <bangkokappdevelopment@gmail.com>
2024-07-22 17:32:14 +0700

    Configured Hardhat config ts file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,18 @@
 import { HardhatUserConfig } from "hardhat/config";
 import "@nomicfoundation/hardhat-toolbox";
+require("@nomiclabs/hardhat-etherscan");
+require("@openzeppelin/hardhat-upgrades");
+
+require("dotenv").config();
 
 const config: HardhatUserConfig = {
   solidity: "0.8.24",
+  networks: {
+    sepolia: {
+      url: process.env.ALCHEMY_SEPOLIA_URL,
+      accounts: [process.env.SEPOLIA_PRIVATE_KEY as string],
+    },
+  },
 };
 
 export default config;
